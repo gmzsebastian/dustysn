f0cdc096a59f4af6560b27d4c8c041cc7ec213f2
Sebastian <gmz.sebastian@gmail.com>
2025-05-18 17:15:15 -0400

    Fixed test

--- dustysn/tests/test_utils.py
+++ dustysn/tests/test_utils.py
@@ -6,6 +6,6 @@ from dustysn.utils import (calc_distance)
 
 def test_calc_distance():
     # Check that the get data returns None
-    output = calc_distance(0.1)
+    output = calc_distance(0.1).value
     # Check that the output is a float
     assert isinstance(output, float)
